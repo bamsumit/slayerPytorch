8fe0834fa7e1afe4a9b4348f1a2d92182cc0cc87
Luca Della Vedova <luca.dellavedova@nus.edu.sg>
2018-09-24 18:00:18 +0800

    Spike binning implemented and tested

--- src/data_reader.py
+++ src/data_reader.py
@@ -4,9 +4,31 @@ import numpy as np
 
 np_event_type = [('x', np.uint16), ('y', np.uint16), ('p', np.uint8), ('ts', np.uint32)]
 
+# Consider dictionary for easier iteration and better scalability
+class SlayerParams(object):
+
+	def __init__(self):
+		self.t_start = None
+		self.t_end = None
+		self.t_res = None
+		self.time_unit = None
+		self.input_x = None
+		self.input_y = None
+		self.input_channels = None
+
+	def is_valid(self):
+		# Could do more checks here (positive, t_res < t_end - t_start)
+		return (self.t_start != None and self.t_end != None and self.t_res != None and self.time_unit != None and
+			self.input_x != None and self.input_y != None and self.input_channels != None)
+
 class DataReader(object):
 
-	def __init__(self, input_path):
+	# TODO pass parameter structure instead of these numbers
+	def __init__(self, input_path, net_params):
+		self.EVENT_BIN_SIZE = 5
+		if not net_params.is_valid():
+			raise ValueError("Network parameters are not valid")
+		self.net_params = net_params
 		# Get files in folder
 		self.input_files = self.get_input_filenames(input_path)
 		
@@ -20,12 +42,31 @@ class DataReader(object):
 		ts = int.from_bytes(raw_bytes[2:], byteorder='big') & 0x7FFFFF
 		return (raw_bytes[0], raw_bytes[1], raw_bytes[2] >> 7, ts)
 
-	# TO OPTIMIZE, remove iteration, preallocate?
+	# TO OPTIMIZE, remove iteration
 	def read_file(self, file):
 		# Preallocate numpy array
 		file_size = os.stat(file).st_size
-		events = np.ndarray((int(file_size / 5)), dtype=np_event_type)
+		events = np.ndarray((int(file_size / self.EVENT_BIN_SIZE)), dtype=np_event_type)
 		with open(file, 'rb') as input_file:
-			for (index, raw_spike) in enumerate(iter(lambda: input_file.read(5), b'')):
+			for (index, raw_spike) in enumerate(iter(lambda: input_file.read(self.EVENT_BIN_SIZE), b'')):
 				events[index] = self.process_event(raw_spike)
 		return events
+
+	# NOTE! Matlab version loads positive spikes first, Python version loads negative spikes first
+	def bin_spikes(self, raw_spike_array):
+		n_inputs = self.net_params.input_x * self.net_params.input_y * self.net_params.input_channels
+		n_timesteps = len(range(self.net_params.t_start,self.net_params.t_end,self.net_params.t_res))
+		binned_array = np.zeros((n_inputs, n_timesteps), dtype=np.uint8)
+		# print(binned_array.shape)
+		# Now do the actual binning
+		for ev in raw_spike_array:
+			# TODO cleanup, access by name (ts) not index
+			ev_x = ev[0]
+			ev_y = ev[1]
+			ev_p = ev[2]
+			ev_ts = ev[3]
+			time_position = int(float(ev_ts) / self.net_params.time_unit)
+			# TODO do truncation if ts over t_end, checks on x and y
+			input_position = ev_p * (self.net_params.input_x * self.net_params.input_y) + (ev_y * self.net_params.input_x) + ev_x
+			binned_array[input_position, time_position] = 1
+		return binned_array
--- test/test_data_reader.py
+++ test/test_data_reader.py
@@ -5,9 +5,20 @@ CURRENT_TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 
 sys.path.append(CURRENT_TEST_DIR + "/../src")
 
-from data_reader import DataReader
+from data_reader import DataReader, SlayerParams
 import csv
+import operator
 import unittest
+import numpy as np
+# Parameters for NMNIST
+net_params = SlayerParams()
+net_params.t_start = 0
+net_params.t_end = 350
+net_params.t_res = 1
+net_params.time_unit = 1000 # How many units in one t_res (i.e. 1000 if we sample in microseconds but simulate at millisecond steps)
+net_params.input_x = 34
+net_params.input_y = 34
+net_params.input_channels = 2
 
 def matlab_equal_to_python_event(matlab_event, python_event):
 	# Cast to avoid type problems
@@ -16,33 +27,54 @@ def matlab_equal_to_python_event(matlab_event, python_event):
 	# Matlab is 1 indexed, Python is 0 indexed
 	return ((matlab_event[0] == (python_event[0] + 1)) and (matlab_event[1] == (python_event[1] + 1)) and
 		(matlab_event[2] == (python_event[2] + 1)) and (matlab_event[3] == (python_event[3])))
+ 
+def binned_file_comparator(matlab_bin_line, python_bin_line):
+	for (matlab_entry, python_entry) in zip(matlab_bin_line, python_bin_line):
+		if int(matlab_entry) != int(python_entry):
+			return False
+	return True
+
+# Utility function to compare ndarray to one contained in CSV file generated separately (i.e. MATLAB)
+def is_array_equal_to_file(array, filepath, has_header=False, compare_function=operator.eq):
+	with open(CURRENT_TEST_DIR + filepath, 'r') as csvfile:
+		reader = csv.reader(csvfile)
+		# Skip header
+		if has_header: next(reader, None)
+		for (g_truth, read_r) in zip(reader, array):
+			if not compare_function(g_truth, read_r):
+				return False
+	return True
 
 class TestDataReaderFolders(unittest.TestCase):
 
 	def test_open_nonexisting_folder(self):
-		self.assertRaises(FileNotFoundError, DataReader, "nonexisting_folder")
+		self.assertRaises(FileNotFoundError, DataReader, "nonexisting_folder", net_params)
 
 	def test_open_valid_folder(self):
 		try:
-			reader = DataReader(CURRENT_TEST_DIR + "/test_files/input_data")
+			reader = DataReader(CURRENT_TEST_DIR + "/test_files/input_data", net_params)
 		except FileNotFoundError:
 			self.fail("Valid input folder not found")
 
 	def test_number_of_files_invalid_folder(self):
 		# Should return 0 files valid
-		reader = DataReader(CURRENT_TEST_DIR + "/test_files/input_validate")
+		reader = DataReader(CURRENT_TEST_DIR + "/test_files/input_validate", net_params)
 		self.assertEqual(len(reader.input_files), 0)
 
 	def test_input_files_ordering(self):
 		file_folder = CURRENT_TEST_DIR + "/test_files/input_data/"
-		reader = DataReader(file_folder)
+		reader = DataReader(file_folder, net_params)
 		self.assertEqual(reader.input_files[0], file_folder + '1.bs2')
+
+	def test_init_invalid_network_params(self):
+		invalid_params = SlayerParams()
+		self.assertRaises(ValueError, DataReader, CURRENT_TEST_DIR + "/test_files/input_data", invalid_params)
 
 
 class TestDataReaderFunc(unittest.TestCase):
 
 	def setUp(self):
-		self.reader = DataReader(CURRENT_TEST_DIR + "/test_files/input_data")
+		self.reader = DataReader(CURRENT_TEST_DIR + "/test_files/input_data", net_params)
 
 	def test_number_of_files_valid_folder(self):
 		self.assertEqual(len(self.reader.input_files), 2)
@@ -60,12 +92,12 @@ class TestDataReaderFunc(unittest.TestCase):
 	# Check proper I/O
 	def test_read_input_file(self):
 		ev_array = self.reader.read_file(self.reader.input_files[0])
-		with open(CURRENT_TEST_DIR + "/test_files/input_validate/1.csv", 'r') as csvfile:
-			reader = csv.reader(csvfile)
-			# Skip header
-			next(reader, None)
-			for (g_truth, read_r) in zip(reader, ev_array):
-				self.assertTrue(matlab_equal_to_python_event(g_truth, read_r))
+		self.assertTrue(is_array_equal_to_file(ev_array, "/test_files/input_validate/1_raw_spikes.csv", has_header=True, compare_function=matlab_equal_to_python_event))
+
+	def test_spikes_binning(self):
+		ev_array = self.reader.read_file(self.reader.input_files[0])
+		binned_spikes = self.reader.bin_spikes(ev_array)
+		self.assertTrue(is_array_equal_to_file(binned_spikes, "/test_files/input_validate/1_binned_spikes.csv", compare_function=binned_file_comparator))
 
 
 if __name__ == '__main__':
