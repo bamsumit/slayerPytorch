16ad48b5ed943bdc3f497c13aede13cac71da908
Luca Della Vedova <luca.dellavedova@nus.edu.sg>
2018-10-04 15:47:30 +0800

    Added custom autograd function for spike, added example of network with two fully connected layers with validated forward propagation

--- src/slayer_train.py
+++ src/slayer_train.py
@@ -5,6 +5,96 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class SlayerNet(nn.Module):
+
+    def __init__(self, net_params, weights_init = [4,4]):
+        super(SlayerNet, self).__init__()
+        self.net_params = net_params
+        self.trainer = SlayerTrainer(net_params)
+        self.srm = self.trainer.calculate_srm_kernel()
+        self.ref = self.trainer.calculate_ref_kernel()
+        # Emulate a fully connected 250 -> 25
+        self.fc1 = nn.Conv3d(1, 25, (1,250,1), bias=False)
+        nn.init.normal_(self.fc1.weight, mean=0, std=weights_init[0])
+        # Emulate a fully connected 25 -> 1
+        self.fc2 = nn.Conv3d(1, 1, (1,25,1), bias=False)
+        nn.init.normal_(self.fc2.weight, mean=0, std=weights_init[1])
+
+    def forward(self, x):
+        # x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        # Apply srm to input spikes
+        x = self.trainer.apply_srm_kernel(x, self.srm)
+        # Linear + activation
+        x = SpikeFunc.apply(self.fc1(x), self.net_params, self.ref, self.net_params['af_params']['sigma'][0])
+        # Apply srm to middle layer spikes
+        x = self.trainer.apply_srm_kernel(x.view(1,1,1,25,501), self.srm)
+        # # Apply second layer
+        x = SpikeFunc.apply(self.fc2(x), self.net_params, self.ref, self.net_params['af_params']['sigma'][1])
+        return x
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+class SpikeFunc(torch.autograd.Function):
+
+	@staticmethod
+	def forward(ctx, multiplied_activations, net_params, ref, sigma):
+		# Calculate membrane potentials
+		# TODO reduce number of saved params 
+		(potentials, spikes) = SpikeFunc.calculate_membrane_potentials(multiplied_activations, net_params, ref, sigma)
+		ctx.save_for_backward(potentials)
+		return spikes
+
+	@staticmethod
+	def backward(ctx, grad_output, net_params):
+		(membrane_potentials, net_params) = ctx.saved_tensors
+		return SpikeFunc.calculate_pdf(membrane_potentials)
+
+	@staticmethod
+	def apply_weights(activations, weights):
+		applied = F.conv3d(activations, weights)
+		return applied
+
+	# Input is potentials after matrix multiplication
+	@staticmethod
+	def calculate_membrane_potentials(potentials, net_params, ref, sigma):
+		# Need float32 to do convolution later
+		spikes = torch.zeros(potentials.shape, dtype=torch.float32)
+		ref_length = len(ref)
+		# Iterate over timestamps, NOTE, check if iterating in this dimension is a bottleneck
+		for p in range(potentials.shape[-1]):
+			ts_pots = potentials[:,:,:,:,p]
+			spike_positions = (ts_pots > net_params['af_params']['theta']).to(dtype=torch.float32)
+			spike_values = spike_positions / net_params['t_s']
+			# Assign output spikes
+			spikes[:,:,:,:,p] = spike_values
+			num_spikes = torch.sum(spike_positions, dim=(1,2,3))
+			have_spike_response = ref * (1 + sigma * (num_spikes - 1))
+			no_spike_response = ref * (sigma * num_spikes)
+			# Now iterate over neurons, apply refractory response
+			for n_id in range(spikes.shape[1]):
+				# Make sure our refractory response doesn't overshoot the total time
+				resp_length = min(potentials.shape[-1] - p, ref_length)
+				if spike_positions[:,n_id,0,0] > 0:
+					# Have spike here
+					# print("got spike")
+					potentials[:,n_id,0,0,p:p+resp_length] += have_spike_response[0:resp_length]
+				else:
+					# Didn't have a spike
+					potentials[:,n_id,0,0,p:p+resp_length] += no_spike_response[0:resp_length]
+			# print(num_spikes)
+		return (potentials, spikes)
+
+	@staticmethod
+	def calculate_pdf(membrane_potentials, net_params):
+		pdf = net_params['pdf_params']['scale'] / net_params['pdf_params']['tau'] * \
+			torch.exp(-abs(membrane_potentials - net_params['af_params']['theta']) / net_params['pdf_params']['tau']) 
+		return pdf
+
 class SlayerTrainer(object):
 
 	def __init__(self, net_params):
@@ -54,42 +144,5 @@ class SlayerTrainer(object):
 		out = F.conv3d(input_spikes, srm, padding=(0,0,int(srm.shape[4]/2))) * self.net_params['t_s']
 		return out
 
-	def apply_weights(self, activations, weights):
-		applied = torch.mm(weights, activations)
-		return applied
-
-	def calculate_membrane_potentials(self, activations, weights, ref, sigma):
-		# Apply weights
-		potentials = self.apply_weights(activations, weights)
-		# Need float32 to do convolution later
-		spikes = torch.zeros(potentials.shape, dtype=torch.float32)
-		ref_length = len(ref)
-		# Iterate over timestamps, NOTE, check if iterating in this dimension is a bottleneck
-		for p in range(potentials.shape[1]):
-			ts_pots = potentials[:,p]
-			spike_positions = ts_pots > self.net_params['af_params']['theta']
-			# Assign output spikes
-			spikes[spike_positions, p] = 1.0 / self.net_params['t_s']
-			num_spikes = int(sum(spike_positions))
-			have_spike_response = ref * (1 + sigma * (num_spikes - 1))
-			no_spike_response = ref * (sigma * num_spikes)
-			# Now iterate over neurons, apply refractory response
-			for n_id in range(len(spike_positions)):
-				# Make sure our refractory response doesn't overshoot the total time
-				resp_length = min(potentials.shape[1] - p, ref_length)
-				if spike_positions[n_id] == 1:
-					# Have spike here
-					potentials[n_id, p:p+resp_length] += have_spike_response[0:resp_length]
-				else:
-					# Didn't have a spike
-					potentials[n_id, p:p+resp_length] += no_spike_response[0:resp_length]
-			# print(num_spikes)
-		return (potentials, spikes)
-
 	def calculate_error_spiketrain(self, a, des_a):
 		return a - des_a
-
-	def calculate_pdf(self, membrane_potentials):
-		pdf = self.net_params['pdf_params']['scale'] / self.net_params['pdf_params']['tau'] * \
-			torch.exp(-abs(membrane_potentials - self.net_params['af_params']['theta']) / self.net_params['pdf_params']['tau']) 
-		return pdf
--- test/test_slayer_train.py
+++ test/test_slayer_train.py
@@ -6,13 +6,20 @@ sys.path.append(CURRENT_TEST_DIR + "/../src")
 
 from data_reader import DataReader, SlayerParams
 from testing_utilities import iterable_float_pair_comparator, iterable_int_pair_comparator, is_array_equal_to_file
-from slayer_train import SlayerTrainer
+from slayer_train import SlayerTrainer, SpikeFunc, SlayerNet
 import unittest
 import os
 from itertools import zip_longest
 import operator
 import numpy as np
 import torch
+
+def read_gtruth_folder(folder):
+		gtruth = {}
+		for file in os.listdir(folder):
+			if file.endswith('.csv'):
+				gtruth[file[0:-4]] = torch.from_numpy(np.genfromtxt(folder + file, delimiter=",", dtype=np.float32))
+		return gtruth
 
 class TestSlayerSRM(unittest.TestCase):
 
@@ -80,16 +87,10 @@ class TestForwardProp(unittest.TestCase):
 		self.trainer = SlayerTrainer(self.net_params)
 		self.srm = self.trainer.calculate_srm_kernel()
 		self.ref = self.trainer.calculate_ref_kernel()
+		self.spike_func = SpikeFunc()
 		self.compare_params = {'FLOAT_EPS_TOL' : 5e-2}
-		self.fprop_gtruth = self.read_gtruth_folder(self.FILES_DIR + "forward_prop/")
-		self.bprop_gtruth = self.read_gtruth_folder(self.FILES_DIR + "back_prop/")
-
-	def read_gtruth_folder(self, folder):
-		gtruth = {}
-		for file in os.listdir(folder):
-			if file.endswith('.csv'):
-				gtruth[file[0:-4]] = torch.from_numpy(np.genfromtxt(folder + file, delimiter=",", dtype=np.float32))
-		return gtruth
+		self.fprop_gtruth = read_gtruth_folder(self.FILES_DIR + "forward_prop/")
+		self.bprop_gtruth = read_gtruth_folder(self.FILES_DIR + "back_prop/")
 
 	def test_ref_kernel_generation(self):
 		self.assertEqual(len(self.ref), 110)
@@ -97,22 +98,28 @@ class TestForwardProp(unittest.TestCase):
 		ref_gtruth = np.genfromtxt(self.FILES_DIR + "refractory_kernel.csv", delimiter=",", dtype=np.float32)
 		self.assertTrue(iterable_float_pair_comparator(self.ref, ref_gtruth, self.compare_params))
 
+	def test_membrane_potential_shape(self):
+		pots = self.spike_func.apply_weights(self.fprop_gtruth['a1'].reshape(1,250,1,1,501), self.fprop_gtruth['W12'].reshape(25,250,1,1,1))
+		(u2, s2) = self.spike_func.calculate_membrane_potentials(pots, self.net_params, self.ref, self.net_params['af_params']['sigma'][0])
+		self.assertEqual(u2.shape, (1,25,1,1,501))
+
+
 	def test_forward_prop_single_sample(self):
 		# Apply SRM to input spikes
 		a1 = self.trainer.apply_srm_kernel(self.fprop_gtruth['s1'].reshape(1,1,1,250,501), self.srm)
 		# Check value
 		self.assertTrue(iterable_float_pair_comparator(a1.flatten(), self.fprop_gtruth['a1'].flatten(), self.compare_params))
 		# Calculate membrane potential and spikes
-		(u2, s2) = self.trainer.calculate_membrane_potentials(a1.reshape(250,501), self.fprop_gtruth['W12'].reshape(25,250), self.ref, 
-			self.net_params['af_params']['sigma'][1])
+		pots = self.spike_func.apply_weights(a1.reshape(1,250,1,1,501), self.fprop_gtruth['W12'].reshape(25,250,1,1,1))
+		(u2, s2) = self.spike_func.calculate_membrane_potentials(pots, self.net_params, self.ref, self.net_params['af_params']['sigma'][0])
 		# Check values
 		self.assertTrue(iterable_float_pair_comparator(u2.flatten(), self.fprop_gtruth['u2'].flatten(), self.compare_params))
 		self.assertTrue(iterable_int_pair_comparator(s2.flatten(), self.fprop_gtruth['s2'].flatten(), self.compare_params))
 		# Just for safety do next layer
 		a2 = self.trainer.apply_srm_kernel(s2.reshape(1,1,1,25,501), self.srm)
 		self.assertTrue(iterable_float_pair_comparator(a2.flatten(), self.fprop_gtruth['a2'].flatten(), self.compare_params))
-		(u3, s3) = self.trainer.calculate_membrane_potentials(a2.reshape(25,501), self.fprop_gtruth['W23'].reshape(1,25), self.ref, 
-			self.net_params['af_params']['sigma'][2])
+		pots = self.spike_func.apply_weights(a2.reshape(1,25,1,1,501), self.fprop_gtruth['W23'].reshape(1,25,1,1,1))
+		(u3, s3) = self.spike_func.calculate_membrane_potentials(pots, self.net_params, self.ref, self.net_params['af_params']['sigma'][1])
 		self.assertTrue(iterable_int_pair_comparator(s3.flatten(), self.fprop_gtruth['s3'].flatten(), self.compare_params))
 		self.assertTrue(iterable_int_pair_comparator(u3.flatten(), self.fprop_gtruth['u3'].flatten(), self.compare_params))
 		# And final activations
@@ -127,9 +134,48 @@ class TestForwardProp(unittest.TestCase):
 		self.assertTrue(iterable_float_pair_comparator(error.flatten(), self.bprop_gtruth['e3'].flatten(), self.compare_params))
 
 	def test_pdf_func(self):
-		pdf = self.trainer.calculate_pdf(self.fprop_gtruth['u3'])
+		pdf = self.spike_func.calculate_pdf(self.fprop_gtruth['u3'], self.net_params)
 		self.assertTrue(iterable_float_pair_comparator(pdf.flatten(), self.bprop_gtruth['rho3'].flatten(), self.compare_params))
 
+
+class TestSlayerNet(unittest.TestCase):
+
+	def setUp(self):
+		self.FILES_DIR = CURRENT_TEST_DIR + "/test_files/torch_validate/"
+		self.net_params = SlayerParams(self.FILES_DIR + "parameters.yaml")
+		self.net = SlayerNet(self.net_params)
+		self.fprop_gtruth = read_gtruth_folder(self.FILES_DIR + "forward_prop/")
+		self.bprop_gtruth = read_gtruth_folder(self.FILES_DIR + "back_prop/")
+		self.trainer = SlayerTrainer(self.net_params)
+		self.srm = self.trainer.calculate_srm_kernel()
+		self.input_spikes = self.fprop_gtruth['s1'].reshape(1,1,1,250,501)
+		des_spikes = self.bprop_gtruth['des_s'].reshape(1,1,1,1,501)
+		self.des_activations = self.trainer.apply_srm_kernel(des_spikes, self.srm)
+		self.compare_params = {'FLOAT_EPS_TOL' : 1e-3}
+
+	def test_forward_pass(self):
+		self.net.fc1.weight = torch.nn.Parameter(self.fprop_gtruth['W12'].reshape(25,1,1,250,1))
+		self.net.fc2.weight = torch.nn.Parameter(self.fprop_gtruth['W23'].reshape(1,1,1,25,1))
+		out_spikes = self.net.forward(self.input_spikes)
+		self.assertEqual(out_spikes.shape, (1,1,1,1,501))
+		self.assertTrue(iterable_float_pair_comparator(self.fprop_gtruth['s3'].flatten(), out_spikes.flatten(), self.compare_params))
+
+
+	# def test_gradients_calculation(self):
+	# 	# Assign weights manually
+	# 	self.net.fc1.weight = torch.nn.Parameter(self.fprop_gtruth['W12'].reshape(25,1,1,250,1))
+	# 	self.net.fc2.weight = torch.nn.Parameter(self.fprop_gtruth['W23'].reshape(1,1,1,25,1))
+	# 	# Forward prop
+	# 	output_spikes = self.net.forward(self.input_spikes)
+
+	# 	print(self.net.fc1.weight.grad)
+
+		
+
+	# def test_training(self):
+	# 	self.input_spikes = self.fprop_gtruth['s1'].reshape(1,1,1,250,501)
+	# 	criterion = torch.nn.MSELoss()
+	# 	optimizer = torch.optim()
 
 
 if __name__ == '__main__':
