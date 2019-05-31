changed weight scaling bug. It was scaled by theta which was not supposed to happen

example/nmnistCNN.py
@@ -11,6 +11,9 @@ import slayerSNN as snn
 from learningStats import learningStats
 
 netParams = snn.params('network.yaml')
+netParams['training']['path']['in']    = '/fast/sumit/NMNIST_34/'
+netParams['training']['path']['train'] = '/fast/sumit/NMNIST_34/train.txt'
+netParams['training']['path']['test']  = '/fast/sumit/NMNIST_34/test.txt'
 
 # Dataset definition
 class nmnistDataset(Dataset):
@@ -64,7 +67,7 @@ class Network(torch.nn.Module):
 # device = torch.device('cuda')
 # Use multiple GPU's if available
 device = torch.device('cuda:2') # should be the first GPU of deviceIDs
-deviceIds = [2, 3]
+deviceIds = [2, 3, 1]
 
 # Create network instance.
 # net = Network(netParams).to(device)
@@ -82,24 +85,24 @@ trainingSet = nmnistDataset(datasetPath =netParams['training']['path']['in'],
 						    sampleFile  =netParams['training']['path']['train'],
 						    samplingTime=netParams['simulation']['Ts'],
 						    sampleLength=netParams['simulation']['tSample'])
-trainLoader = DataLoader(dataset=trainingSet, batch_size=8, shuffle=False, num_workers=4)
+trainLoader = DataLoader(dataset=trainingSet, batch_size=12, shuffle=False, num_workers=4)
 
 testingSet = nmnistDataset(datasetPath  =netParams['training']['path']['in'], 
 						    sampleFile  =netParams['training']['path']['test'],
 						    samplingTime=netParams['simulation']['Ts'],
 						    sampleLength=netParams['simulation']['tSample'])
-testLoader = DataLoader(dataset=testingSet, batch_size=8, shuffle=False, num_workers=4)
+testLoader = DataLoader(dataset=testingSet, batch_size=12, shuffle=False, num_workers=4)
 
 # Learning stats instance.
 stats = learningStats()
 
-# Visualize the network.
-for i in range(5):
-	input, target, label = trainingSet[i]
-	snn.io.showTD(snn.io.spikeArrayToEvent(input.reshape((2, 34, 34, -1)).cpu().data.numpy()))
+# # Visualize the network.
+# for i in range(5):
+# 	input, target, label = trainingSet[i]
+# 	snn.io.showTD(snn.io.spikeArrayToEvent(input.reshape((2, 34, 34, -1)).cpu().data.numpy()))
 
 # training loop
-for epoch in range(10):
+for epoch in range(100):
 	tSt = datetime.now()
 	
     # Training loop.

src/slayer.py
@@ -142,7 +142,7 @@ class spikeLayer(torch.nn.Module):
 		'''
 		return _pspFunction.apply(spike, self.srmKernel, self.simulation['Ts'])
 	
-	def dense(self, inFeatures, outFeatures):
+	def dense(self, inFeatures, outFeatures, weightScale=10):	# default weight scaling of 10
 		'''
 		Retuns a function that can be called to apply dense layer mapping to input tensor per time instance.
 		It behaves similar to ``torch.nn.Linear`` applied for each time instance.
@@ -158,9 +158,9 @@ class spikeLayer(torch.nn.Module):
 		>>> fcl = snnLayer.dense((128, 128, 2), 512) # takes (N, 2, 128, 128, T) tensor
 		>>> output = fcl(input)                      # output will be (N, 512, 1, 1, T) tensor
 		'''
-		return _denseLayer(inFeatures, outFeatures, weightScale=self.neuron['theta'])
+		return _denseLayer(inFeatures, outFeatures, weightScale)	
 		
-	def conv(self, inChannels, outChannels, kernelSize, stride=1, padding=0, dilation=1, groups=1):
+	def conv(self, inChannels, outChannels, kernelSize, stride=1, padding=0, dilation=1, groups=1, weightScale=100):	# default weight scaling of 100
 		'''
 		Returns a function that can be called to apply conv layer mapping to input tensor per time instance.
 		It behaves same as ``torch.nn.conv2d`` applied for each time instance.
@@ -185,7 +185,7 @@ class spikeLayer(torch.nn.Module):
 		>>> conv = snnLayer.conv(2, 32, 5) # 32C5 flter
 		>>> output = conv(input)           # must have 2 channels
 		'''
-		return _convLayer(inChannels, outChannels, kernelSize, stride, padding, dilation, groups, weightScale=self.neuron['theta']**2)
+		return _convLayer(inChannels, outChannels, kernelSize, stride, padding, dilation, groups, weightScale) 
 		
 	def pool(self, kernelSize, stride=None, padding=0, dilation=1):
 		'''
@@ -258,6 +258,7 @@ class _denseLayer(nn.Conv3d):
 
 		if weightScale != 1:	
 			self.weight = torch.nn.Parameter(weightScale * self.weight)	# scale the weight if needed
+			# print('In dense, using weightScale of', weightScale)
 
 	
 	def forward(self, input):
@@ -321,6 +322,7 @@ class _convLayer(nn.Conv3d):
 
 		if weightScale != 1:	
 			self.weight = torch.nn.Parameter(weightScale * self.weight)	# scale the weight if needed
+			# print('In conv, using weightScale of', weightScale)
 
 	def foward(self, input):
 		'''
@@ -377,6 +379,8 @@ class _poolLayer(nn.Conv3d):
 
 		# set the weights to 1.1*theta and requires_grad = False
 		self.weight = torch.nn.Parameter(torch.FloatTensor(1.1 * theta * np.ones((self.weight.shape))).to(self.weight.device), requires_grad = False)
+		# print('In pool layer, weight =', self.weight.cpu().data.numpy().flatten(), theta)
+
 
 	def forward(self, input):
 		'''